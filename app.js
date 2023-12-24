95cedfd583826810d678fd00671b15edffc6b3cb
pachekox <www.pacheko.inc@gmail.com>
2023-12-25 00:32:18 +0300

    rename

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-async function getMovies() {
+async function getJoke() {
 
     const response = await fetch('https://official-joke-api.appspot.com/random_joke')
     const movies = await response.json()
@@ -10,4 +10,8 @@ async function getMovies() {
     document.getElementById('punchline').innerHTML=punchline;
 
 }
-window.onload = getMovies;
+
+copyJoke(){
+
+}
+window.onload = getJoke;
